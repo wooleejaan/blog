5c5c87de900c9382c88436ace0d1f64845e8db56
yongwoo <wooleejaan@gmail.com>
2024-05-24 00:02:44 +0900

    chore: Added packages (astro-pagefind, pagefind)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import pagefind from "astro-pagefind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  build: {
+    format: "file",
+  },
+  integrations: [mdx(), sitemap(), pagefind()],
 });
