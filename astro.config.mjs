4d0196add243024944b602cbaa72fad4d6d0176c
yongwoo <wooleejaan@gmail.com>
2024-05-26 13:12:43 +0900

    chore: Added config & packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 import sitemap from "@astrojs/sitemap";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   build: {
-    format: "file",
+    format: "file"
   },
-  integrations: [mdx(), sitemap(), pagefind()],
+  integrations: [mdx(), sitemap(), pagefind(), react()],
+  devToolbar: {
+    enabled: false
+  }
 });
