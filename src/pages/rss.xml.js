74967f8d1ccbca3698aa2ef15ec57c506b6b3966
yongwoo <wooleejaan@gmail.com>
2024-05-28 10:25:17 +0900

    chore: Added rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,31 @@ import { getCollection } from 'astro:content'
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts'
 
 export async function GET(context) {
-  const posts = await getCollection('note')
+  const posts = await getCollection('post')
+  const notes = await getCollection('note')
+  const collections = await getCollection('collection')
+
+  const getItems = () => {
+    const postItems = posts.map((post) => ({
+      ...post.data,
+      link: `/post/${post.slug}/`,
+    }))
+    const noteItems = notes.map((post) => ({
+      ...post.data,
+      link: `/note/${post.slug}/`,
+    }))
+    const collectionItems = collections.map((post) => ({
+      ...post.data,
+      link: `/collection/${post.slug}/`,
+    }))
+
+    return [...postItems, ...noteItems, ...collectionItems]
+  }
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => ({
-      ...post.data,
-      link: `/note/${post.slug}/`,
-    })),
+    items: getItems(),
   })
 }
