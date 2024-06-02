5b77e1a02cd4feeaf8732135f38ee88cb82d23ee
yongwoo <wooleejaan@gmail.com>
2024-06-02 23:09:48 +0900

    Removed notes section

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,6 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../consts'
 
 export async function GET(context) {
   const posts = await getCollection('engineering')
-  const notes = await getCollection('note')
   const collections = await getCollection('collection')
 
   const getItems = () => {
@@ -12,16 +11,12 @@ export async function GET(context) {
       ...post.data,
       link: `/post/${post.slug}/`,
     }))
-    const noteItems = notes.map((post) => ({
-      ...post.data,
-      link: `/note/${post.slug}/`,
-    }))
     const collectionItems = collections.map((post) => ({
       ...post.data,
       link: `/collection/${post.slug}/`,
     }))
 
-    return [...postItems, ...noteItems, ...collectionItems]
+    return [...postItems, ...collectionItems]
   }
 
   return rss({
